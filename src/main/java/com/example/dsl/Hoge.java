c740ac0186c39e40f509be32b79d4459a88e7846
Toru.Inoue <Toru.Inoue@so-net.co.jp>
2016-05-24 17:22:20 +0900

    test

---
+++ src/main/java/com/example/dsl/Hoge.java
@@ -0,0 +1,12 @@
+package com.example.dsl;
+
+public class Hoge {
+  private String branch;
+  public Hoge(String branch) {
+    this.branch = branch;
+  }
+
+  public String getBranch() {
+    return this.branch;
+  }
+}
